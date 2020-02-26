88d458de6897392e58cfe1468bd3eee99aa9f62c
Balazs-D <balazs.danyadi@gmail.com>
2020-02-26 03:33:02 +0100

    scroll works, seacrh reset needs adjust, nav style needs upgrade

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Home from './Home';
 import Characters from './Characters';
 import Events from './Events';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+const [per, setPer] = useState(0);
+
+const mouseMove = e => {
+  setPer(Math.round((e.screenX / window.innerWidth) * 100));
+  console.log(per);
+};
+
+const goldBg = {
+  background: `linear-gradient(to right, #bf953f ${per -
+    15}%, #fcf6ba ${per}%, #bf953f ${per + 15}%, #bf953f,
+    #aa771c 100%)`,
+  fontSize: '4vw'
+};
+
+
   return (
     <Router>
       <div>
         <nav className='nav-cont'>
-          <ul className='nav-ul'>
+          <ul className='nav-ul' style={goldBg} onMouseMove={mouseMove}>
             <li className='nav-li'>
               <Link className='nav-a' to='/home'>
                 Home
